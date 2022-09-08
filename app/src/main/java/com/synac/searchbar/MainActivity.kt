38983462e98fa6358c9240dd94c9a68d7f55dd09
Mohammad Arif <danielharold21@gmail.com>
2022-09-08 07:44:15 +0530

    Initial commit

---
+++ app/src/main/java/com/synac/searchbar/MainActivity.kt
@@ -0,0 +1,82 @@
+package com.synac.searchbar
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.synac.searchbar.ui.theme.SearchBarTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SearchBarTheme {
+
+            }
+        }
+    }
+}
+
+val actorsList = listOf(
+    "Leonardo DiCaprio",
+    "Chris Evans",
+    "Brad Pitt",
+    "Elizabeth Olsen",
+    "Kate Winslet",
+    "Tom Holland",
+    "Joseph Gordon-Levitt",
+    "Scarlett Johansson",
+    "Anne Hathaway",
+    "Meryl Streep",
+    "Tom Hardy",
+    "Tom Cruise",
+    "Sandra Bullock",
+    "Charlize Theron",
+    "Dakota Johnson",
+    "James Franco",
+    "Paul Rudd",
+    "Jennifer Lawrence",
+    "Benedict Cumberbatch",
+    "Hugh Jackman",
+    "Tom Hiddleston",
+    "Anna Kendrick",
+    "Daniel Craig",
+    "Shah Rukh Khan",
+    "Will Smith",
+    "George Clooney",
+    "Liam Neeson",
+    "Angelina Jolie",
+    "Michael Fassbender",
+    "Idris Elba",
+    "Russell Crowe",
+    "Ryan Gosling",
+    "Ben Affleck",
+    "Chris Hemsworth",
+    "Margot Robbie",
+    "Emma Stone",
+    "Natalie Portman",
+    "Tom Hanks",
+    "Denzel Washington",
+    "Mark Wahlberg",
+    "Matt Damon",
+    "Chris Pratt",
+    "Samuel L. Jackson",
+    "Johnny Depp",
+    "Robert Downey Jr",
+    "Christian Bale",
+    "Matthew McConaughey",
+    "Morgan Freeman",
+    "Jake Gyllenhaal",
+    "Jeremy Renner",
+    "Dwayne Johnson",
+    "Michael B. Jordan",
+    "Mark Ruffalo",
+    "Jesse Eisenberg",
+    "Keanu Reeves",
+)
